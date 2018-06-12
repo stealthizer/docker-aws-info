0a700604ab8e43c41a2f73729a9fde7d983fc17d
Jordi Franco <jordi.franco@scmspain.com>
2018-06-12 21:59:47 +0200

    added templates to flask. Fixed a bug in ondemand searching on lifecycle key

--- app/app.py
+++ app/app.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask, render_template
 import boto3
 import requests
 
 
 app = Flask(__name__)
 
-def describe_instances(instance_id):
+def describe_instances(instanceId):
     client = boto3.client('ec2', region_name='eu-west-1')
     try:
-        instances = client.describe_instances(InstanceIds=[instance_id])
+        instances = client.describe_instances(InstanceIds=[instanceId])
         return instances
     except Exception as e:
         return e
@@ -17,17 +17,19 @@ def describe_instances(instance_id):
 def get_instance_id():
     response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
     data = response.text
-    print(data)
     return data
 
 @app.route('/')
 def aws_info():
-    instance_id = get_instance_id()
-    instance_data = describe_instances(instance_id)['Reservations'][0]['Instances'][0]
+    instanceId = get_instance_id()
+    instance_data = describe_instances(instanceId)['Reservations'][0]['Instances'][0]
     instanceType = instance_data['InstanceType']
-    instanceLifecycle = instance_data['InstanceLifecycle']
-    html_code = '<table><td><tr>' + instance_id + '<br>' + instanceType + '<br>' + instanceLifecycle + '</tr></td></table>'
-    return html_code
+    try:
+        instanceLifecycle = instance_data['InstanceLifecycle']
+    except:
+        instanceLifecycle = 'normal'
+    return render_template('instance_template.html', instanceId=instanceId, instanceType=instanceType,
+                           instanceLifecycle=instanceLifecycle)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
